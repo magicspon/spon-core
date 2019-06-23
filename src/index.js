710d942ea384c825b57e2f900beea8133791ccea
Dave Stockley <magicspon@gmail.com>
2019-06-23 23:42:39 +0100

    remove plugins and connect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 export { default as loadApp, registerPlugin, eventBus, cache } from './app'
-
-// import { registerPlugin } from './app'
-// import bindPlugins from './modules/withPlugins'
-
-// export const withPlugins = bindPlugins(registerPlugin)
-
-// import connect from './modules/connect'
-
-// export const connectStore = connect(registerPlugin)
